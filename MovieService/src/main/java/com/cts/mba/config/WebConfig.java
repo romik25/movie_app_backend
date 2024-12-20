801fe43f3d529619a290e49f1e6081107452fc78
Romik <romikbanerjee2512@gmail.com>
2024-12-20 14:52:15 +0530

    added * in allowed origins for web config files

--- MovieService/src/main/java/com/cts/mba/config/WebConfig.java
+++ MovieService/src/main/java/com/cts/mba/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://zealous-mushroom-03d1a2310.4.azurestaticapps.net")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
