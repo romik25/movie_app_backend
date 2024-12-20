8fb6654ddf05e9c9bfd64e5a3db267a20a144b79
Romik <romikbanerjee2512@gmail.com>
2024-12-20 14:13:09 +0530

    adding global CORS web config file to all microservices

---
+++ AdminMovieService/src/main/java/com/cts/mba/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.cts.mba.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://zealous-mushroom-03d1a2310.4.azurestaticapps.net")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true);
+    }
+} 
